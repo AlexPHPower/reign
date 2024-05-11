942e0d5367eae80baed4facd39639103e97d3b05
AlexPHPower <bearhug@alexpower.co.uk>
2024-05-11 19:56:57 +0100

    Added dropdown menu to login button:

--- src/app/_components/signUpButton.tsx
+++ src/app/_components/signUpButton.tsx
@@ -2,6 +2,23 @@ import { signOut, useSession, signIn } from "next-auth/react";
 import Image from "next/image";
 import { createAvatar, type Style } from "@dicebear/core";
 import { identicon } from "@dicebear/collection";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import {
+  CreditCard,
+  Keyboard,
+  LogOut,
+  Settings,
+  User,
+  LayoutDashboard,
+} from "lucide-react";
 
 const identiconFunction = identicon as Style<{ seed: string }>;
 
@@ -14,20 +31,52 @@ export default function SignUpButton() {
 
     const placeholder = `data:image/svg+xml;utf8,${encodeURIComponent(avatarSvg.toString())}`;
 
-    console.log(placeholder);
     return (
-      <button
-        onClick={() => signOut()}
-        className="mt-4 block px-4 py-2 underline-offset-4 hover:underline lg:mt-0 lg:inline-block"
-      >
-        <Image
-          src={session?.user?.image ?? placeholder}
-          alt="User Image"
-          className="rounded-full"
-          width={44}
-          height={44}
-        />
-      </button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className="mt-4 block px-4 py-2 lg:mt-0 lg:inline-block">
+            <Image
+              src={session?.user?.image ?? placeholder}
+              alt="User Image"
+              className="rounded-full"
+              width={44}
+              height={44}
+            />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <LayoutDashboard className="mr-2 h-4 w-4" />
+              <span>Dashboard</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <button
+              onClick={() => signOut()}
+              className="inline-flex items-center"
+            >
+              <LogOut className="mr-2 h-4 w-4" />
+              Log out
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     );
   } else {
     return (
