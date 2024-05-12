175d4fdabc89658aedc47d74ac95ff9263f9dbc5
AlexPHPower <bearhug@alexpower.co.uk>
2024-05-12 18:03:11 +0100

    dashboard component

---
+++ src/components/dashboard/userTableRow.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { TableCell, TableRow } from "~/components/ui/table";
+import Image from "next/image";
+import { Badge } from "~/components/ui/badge";
+import * as React from "react";
+import { getUserImage } from "~/lib/utils";
+import { type User } from "next-auth";
+
+interface UserTableRowProps {
+  user: User;
+}
+
+const UserTableRow: React.FC<UserTableRowProps> = ({ user }) => {
+  const image = getUserImage(user);
+
+  return (
+    <TableRow>
+      <TableCell className="hidden sm:table-cell">
+        <Image
+          alt="Product image"
+          className="rounded-full object-cover"
+          height="44"
+          src={image}
+          width="44"
+        />
+      </TableCell>
+      <TableCell className="font-medium">Laser Lemonade Machine</TableCell>
+      <TableCell>
+        <Badge variant="outline">Draft</Badge>
+      </TableCell>
+      <TableCell>$499.99</TableCell>
+      <TableCell className="hidden md:table-cell">25</TableCell>
+      <TableCell className="hidden md:table-cell">
+        2023-07-12 10:42 AM
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default UserTableRow;
