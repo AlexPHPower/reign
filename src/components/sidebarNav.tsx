a195549eaa87ce68bfe7c10e34fc57ebbe021e0c
AlexPHPower <bearhug@alexpower.co.uk>
2024-05-17 20:08:02 +0100

    Made changes to sidenav

--- src/components/sidebarNav.tsx
+++ src/components/sidebarNav.tsx
@@ -1,13 +1,5 @@
 import Link from "next/link";
-import {
-  Home,
-  AreaChart,
-  Receipt,
-  LineChart,
-  Package2,
-  Settings,
-  Table,
-} from "lucide-react";
+import { Home, AreaChart, Receipt, Crown, Settings, Table } from "lucide-react";
 import {
   Tooltip,
   TooltipContent,
@@ -15,32 +7,20 @@ import {
   TooltipTrigger,
 } from "~/components/ui/tooltip";
 import React from "react";
+import { Separator } from "~/components/ui/separator";
 
 export default function SidebarNav() {
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
         <Link
-          href="#"
+          href="/"
           className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
-          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+          <Crown className="h-4 w-4 transition-all group-hover:scale-110" />
           <span className="sr-only">Acme Inc</span>
         </Link>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="/"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Home className="h-5 w-5" />
-                <span className="sr-only">Home</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Home</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Separator />
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger asChild>
